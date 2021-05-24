dd54ad8e336a94eb839c32f21d56f2fdb7a34cb6
Karishma Shaik <76698398+karishma283@users.noreply.github.com>
2021-05-25 00:00:16 +0200

    EXERCISE 1

---
+++ d4.js
@@ -0,0 +1,15 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript (uncommented)
+- You can ask for help, reach the Teaching Assistants if needed
+- The solution must be available for the tutors by the end of the day (5PM CET)
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+*/
+
+/* EXERCISE 1
+Create an array containing the first 5 positive numbers.
+*/
+let positive =[2,4,6,8,10]
+console.log(positive)
